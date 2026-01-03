db76e4ed3348ef5a57f1182124e91ed3f3e847b9
dhanushya2007 <dhanushya.r2024cse@sece.ac.in>
2026-01-03 19:38:26 +0530

    UI for all pages

--- frontend/src/firebase/authService.js
+++ frontend/src/firebase/authService.js
@@ -3,21 +3,32 @@ import { auth, provider } from "./firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
+let loginPromise = null;
+
 export const googleLogin = async () => {
-  const result = await signInWithPopup(auth, provider);
-  const user = result.user;
-  // domain check
-  if (!user.email.endsWith("@sece.ac.in")) {
-    await signOut(auth);
-    alert("Use @sece.ac.in email only");
-    throw new Error("Invalid domain");
+  if (loginPromise) return loginPromise;
+
+  loginPromise = (async () => {
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    if (!user.email.endsWith("@sece.ac.in")) {
+      await signOut(auth);
+      throw new Error("Use @sece.ac.in email only");
+    }
+
+    const snap = await getDoc(doc(db, "users", user.uid));
+
+    return {
+      user,
+      isNewUser: !snap.exists(),
+      role: snap.exists() ? snap.data().role : null
+    };
+  })();
+
+  try {
+    return await loginPromise;
+  } finally {
+    loginPromise = null;
   }
-
-  const snap = await getDoc(doc(db, "users", user.uid));
-
-  return {
-    user,
-    isNewUser: !snap.exists(),
-    role: snap.exists() ? snap.data().role : null
-  };
 };
